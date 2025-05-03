4ebc0dca2be529c3bff757233c551588f1cf508a
blowsnow <im.blowsnow@gmail.com>
2025-05-03 13:36:06 +0800

    feat: 样式优化

--- .scripts/build.js
+++ .scripts/build.js
@@ -1,11 +1,12 @@
 // 1.扫描上级目录所有
 const fs = require('fs');
 const path = require('path');
-const {buildProjectInfo, projectJsonPath, buildProjects} = require("./utils");
+const {buildProjectInfo, buildProjects} = require("./utils");
 
 
 const list = fs.readdirSync(path.join(__dirname, '../')).filter(dir => {
     if (dir.startsWith(".")) return false
+    if (dir === "") return false;
     // 判断是否是目录
     const stat = fs.statSync(path.join(__dirname, '../', dir));
     if (!stat.isDirectory()) return false;
